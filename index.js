26b7056c88f3d30ac6be69f3e7b7ee9deceeb862
Won Kim <wonsae.kim@gmail.com>
2019-06-03 12:49:07 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(drivers) {
+    return firstTwoDrivers(drivers);
+}
+  
+const firstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+}
+  
+function returnLastTwoDrivers(drivers) {
+    return lastTwoDrivers(drivers);
+}
+  
+const lastTwoDrivers = function(drivers) {
+    return drivers.slice(drivers.length - 2, drivers.length)
+}
+  
+const selectingDrivers = [
+    returnFirstTwoDrivers,
+    returnLastTwoDrivers
+];
+
+const createFareMultiplier = function(multi) {
+    return function(num) {
+        return num * multi;
+    }
+}
+
+const fareDoubler = function(num) {
+    return num * 2;
+}
+
+const fareTripler = function(num) {
+    return num * 3;
+}
+
+function selectDifferentDrivers(array, func) {
+    return func(array);
+}
